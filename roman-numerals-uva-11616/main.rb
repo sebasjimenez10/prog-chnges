433190b1850d68057bf4d0f7ad1cbba76dbd8198
Sebastian Jimenez <sebasjimenezv@gmail.com>
2014-06-17 01:50:53 -0500

    Finally adding the main program to read and write, input and output respectively

---
+++ roman-numerals-uva-11616/main.rb
@@ -0,0 +1,33 @@
+require_relative 'roman_numerals'
+
+#############################################
+# Main program for Roman Numerals Challenge #
+#############################################
+
+# Open input file
+inputFile = File.new("roman_numerals_input.txt", "r")
+
+# Create output file
+outputFile = File.new("roman_numerals_output.txt", "w")
+
+# Create the magic
+rn = RomanNumerals.new
+
+# Create result array
+results = Array.new
+
+
+while( line = inputFile.gets )
+	
+	puts "Input line: ".concat( line )
+
+	outputLine = rn.convert( line.chomp )
+	outputFile.puts( outputLine )
+
+	results.push( outputLine )
+
+end
+
+results.each do | r |
+	puts "Output line: ".concat( r )
+end
